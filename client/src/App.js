2db8305f5cd9f84d1917d6e9c7ff19fac4d043ff
Sanket <sanketsharma241@gmail.com>
2024-02-10 11:36:40 +0530

    set carousel and multi carousel

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
-
 import './App.css';
+import Footer from './component/footer/Footer';
 import Navbar from './component/header/Navbar';
 import MainComp from './component/home/MainComp';
 import Newnav from './component/newnav/Newnav';
+import { Route, Routes } from 'react-router-dom';
+import Signup from './component/signup_signin/Signup';
+import Signin from './component/signup_signin/Signin';
 
 function App() {
   return (
     <>
-
       < Navbar />
       <Newnav />
-      <MainComp />
+      <Routes >
+        <Route path='/' element={<MainComp />} />
+        <Route path='/register' element={<Signup />} />
+        <Route path='/login' element={<Signin />} />
+
+      </Routes>
+      <Footer />
+
 
 
     </>
--- client/src/component/home/MainComp.jsx
+++ client/src/component/home/MainComp.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Home.css'
 import Banner from './Banner'
+import Slide from './Slide'
 const MainComp = () => {
     return (
         <>
@@ -8,6 +9,24 @@ const MainComp = () => {
                 <div className="banner_part">
                     <Banner />
                 </div>
+                <div className="slide_part">
+                    <div className="left_slide">
+                        <Slide />
+
+                    </div>
+                    <div className="right_slide">
+                        <h4>Festive latest launches</h4>
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/Jupiter/Launches/T3/DesktopGateway_CategoryCard2x_758X608_T3._SY608_CB639883570_.jpg" alt="rightimg" />
+                        <a href="#">See more</a>
+                    </div>
+                </div>
+                <Slide title="Today's Deal" />
+                <div className="center_img">
+                    <img src="https://m.media-amazon.com/images/G/31/AMS/IN/970X250-_desktop_banner.jpg" alt="" />
+                </div>
+                <Slide title="Best Seller" />
+                <Slide title="upto 80% off" />
+
             </div>
         </>
     )
